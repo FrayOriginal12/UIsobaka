ef1d11524b56ce636e5093af466e5f25698c6f5c
FrayOriginal12 <99204053+FrayOriginal12@users.noreply.github.com>
2023-04-07 15:51:23 +0300

    Ui.lua

---
+++ Ui.lua
@@ -0,0 +1,60 @@
+local GameScriptGui = Instance.new("ScreenGui")
+local MainFrame = Instance.new("ImageLabel")
+local corner = Instance.new("UICorner")
+
+GameScriptGui.Name = "GameScriptGui"
+GameScriptGui.Parent = game.CoreGui
+
+
+
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = GameScriptGui
+MainFrame.Image = "rbxassetid://12880539722"
+MainFrame.Active = true
+MainFrame.Draggable = true
+MainFrame.Position = UDim2.new(0.339118838, 0, 0.0474308431, 0)
+MainFrame.Size = UDim2.new(0, 564, 0, 304)
+corner.Parent = MainFrame
+MainFrame.BackgroundTransparency = 1
+MainFrame.ImageTransparency = 1
+
+wait(0.01)
+MainFrame.ImageTransparency = 0.9
+wait(0.01)
+MainFrame.ImageTransparency = 0.8
+wait(0.01)
+MainFrame.ImageTransparency = 0.7
+wait(0.01)
+MainFrame.ImageTransparency = 0.6
+wait(0.01)
+MainFrame.ImageTransparency = 0.5
+wait(0.01)
+MainFrame.ImageTransparency = 0.4
+wait(0.01)
+MainFrame.ImageTransparency = 0.3
+wait(0.01)
+MainFrame.ImageTransparency = 0.2
+wait(0.01)
+MainFrame.ImageTransparency = 0.1
+wait(0.01)
+MainFrame.ImageTransparency = 0
+wait(1)
+MainFrame.ImageTransparency = 0.1
+wait(0.01)
+MainFrame.ImageTransparency = 0.2
+wait(0.01)
+MainFrame.ImageTransparency = 0.3
+wait(0.01)
+MainFrame.ImageTransparency = 0.4
+wait(0.01)
+MainFrame.ImageTransparency = 0.5
+wait(0.01)
+MainFrame.ImageTransparency = 0.6
+wait(0.01)
+MainFrame.ImageTransparency = 0.7
+wait(0.01)
+MainFrame.ImageTransparency = 0.8
+wait(0.01)
+MainFrame.ImageTransparency = 0.9
+wait(0.01) 
+MainFrame.ImageTransparency = 1
